5c4735b02f39541f853a347c126820b099b8132b
Memohat <49382701+Memohat@users.noreply.github.com>
2019-06-26 17:05:09 -0400

    Add files via upload
    Logging, percent display, refining user experience

--- download.py
+++ download.py
@@ -1,32 +1,36 @@
 #!python3
 # Mehmet Hatip
+try:
+    import os, praw, imgurpython, logging, configparser, sys, requests, regex
+    import subprocess
+except e as Exception:
+    print(f'Error: {e}')
+    sys.exit()
 
-import os, praw, imgurpython, logging, configparser, sys, requests, regex
-
-def log_start():
-    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
-    #logging.disable(logging.CRITICAL)
-
-def clients():
+def clients(name, config=None):
     try:
-        config = configparser.ConfigParser()
-        config.read('client_info.ini')
-
-        reddit = praw.Reddit(
-        client_id = config['reddit']['client_id'],
-        client_secret = config['reddit']['client_secret'],
-        user_agent = config['reddit']['user_agent']
-        )
-        imgur = imgurpython.ImgurClient(
-        client_id = config['imgur']['client_id'],
-        client_secret = config['imgur']['client_secret']
-        )
-
-        return reddit, imgur
+        if not config:
+            filename = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), 'settings.ini')
+            )
+            config = configparser.ConfigParser()
+            config.read(filename)
+        if name == 'reddit':
+            reddit = praw.Reddit(
+            client_id = config['reddit']['client_id'],
+            client_secret = config['reddit']['client_secret'],
+            user_agent = config['reddit']['user_agent']
+            )
+            return reddit
+        elif name == 'imgur':
+            imgur = imgurpython.ImgurClient(
+            client_id = config['imgur']['client_id'],
+            client_secret = config['imgur']['client_secret']
+            )
+            return imgur
     except Exception as e:
         print(f'Error: {e}')
         sys.exit()
-
 
 def find_extension(url):
     try:
@@ -35,7 +39,7 @@ def find_extension(url):
     except:
         return None
 
-def slim_title(title, limit):
+def slim_title(title, limit=250):
     name = regex.sub(r"[^\s\w',]", '', title).strip()
     char_max = limit - len(os.path.abspath('.'))
     name = name[:char_max-1] if len(name) >= char_max else name
@@ -59,34 +63,30 @@ def gfycat_url(url):
         pass
     return url
 
-def imgur_album(id):
+def imgur_album(title, id):
+    imgur = clients('imgur')
+    make_dir(title)
     images = imgur.get_album_images(id)
-    logging.debug(f'Downloading imgur album')
-    i = 1
+    logging.info(f'Downloading imgur album')
 
     for item in images:
         imgur_image(item=item)
 
-    logging.debug('\nFinished imgur album')
+    os.chdir('..')
+    logging.info('\nFinished imgur album')
 
-def imgur_image(id=None, item=None):
-    i = 1
-    try:
-        item = imgur.get_image(id)
-    except:
-        pass
+def imgur_image(title, id=None, item=None):
+    imgur = clients('imgur')
+    item = imgur.get_image(id) if id else item
     if item.animated:
         url = item.mp4
     else:
         url = item.link
     if item.title:
         title = item.title
-    else:
-        title = 'Untitled' + str(i)
-        i += 1
     extension = find_extension(url)
     status = download_file(title + extension, url)
-    print(status)
+    logging.info(status)
 
 def subreddit_param(sub, section, posts):
     if section == 'top':
@@ -95,38 +95,65 @@ def subreddit_param(sub, section, posts):
         return sub.hot(limit=posts)
     elif section == 'new':
         return sub.new(limit=posts)
+    elif section == 'cont':
+        return sub.controversial(limit=posts)
 
 def make_dir(dir_name):
     if not os.path.isdir(dir_name):
         os.mkdir(dir_name)
     os.chdir(dir_name)
 
-def download_subreddit(sub_name='pics', section='top', posts=10):
-    reddit, imgur = clients()
+def get_size(storage, start_path=os.getcwd()):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                try:
+                    total_size += os.path.getsize(fp)
+                except:
+                    None
+    max_gigs = storage * 10 ** 9
+    logging.info(f'get_size percent: \'{round(total_size / max_gigs * 100)}\'')
+    if total_size >= max_gigs:
+        return 100
+    else:
+        percent = round(total_size / max_gigs * 100)
+        return percent
+
+def download_subreddit(sub_name, section, posts, storage):
+    reddit = clients('reddit')
     try:
         if sub_name == 'r':
             sub = reddit.random_subreddit()
         else:
             sub = reddit.subreddit(sub_name)
-        name = sub.display_name
+        sub_name = sub.display_name
         title = sub.title
         if sub.over18:
             raise Exception('Nice try...')
     except Exception as e:
-        print(f'Error: {e}')
-        return
+        print(f'Error: {e}\nDoes the subreddit exist?')
+        return False
 
-    logging.debug('Subreddit downloaded')
+    logging.info(f'\nSubreddit {sub_name} downloaded\n')
+    make_dir(sub_name)
+    print(f"Downloading {sub_name}...\nTitle: {title}")
 
-    make_dir(sub.display_name)
-    os.startfile('.')
-
+    percent, i = 0, 0
+    gigs_percent = get_size(storage)
+    if gigs_percent >= 100:
+        sys.stdout.write(f"\r100%")
+        print(f'\n{storage} gigabytes reached\n{i} posts downloaded')
+        return True
     for submission in subreddit_param(sub, section, posts):
         try:
+            sys.stdout.write(f"\r{percent}%")
             if submission.over_18:
                 raise Exception('Nice try...')
             url = submission.url
-            title = slim_title(submission.title, 250)
+            title = slim_title(submission.title)
             text = submission.selftext
             extension = find_extension(url)
 
@@ -135,52 +162,64 @@ def download_subreddit(sub_name='pics', section='top', posts=10):
                 raise Exception('File already exists')
 
             # logging
-            logging.info("\nINFO\nInitial URL: " + str(url))
-            logging.info("ID: " + submission.id)
+            logging.info(
+            f"\nINFO\n\t" +
+            f"Initial URL: {str(url)}\n\t" +
+            f"ID: {submission.id}\n\t" +
+            f"Title: {title}")
 
-            if submission.is_reddit_media_domain and submission.is_video:
-                url = submission.media['reddit_video']['fallback_url']
-                if submission.media['reddit_video']['is_gif']:
-                    extension = '.mp4'
-                else:
-                    url_audio = regex.sub(r'\/[^\/]+$',r'/audio', url)
-                    status = download_video(title, url, url_audio)
-                    logging.info(status)
-                    continue
-            elif bool(regex.search(r'streamable\.com\/\w+', url)):
-                url = streamable_url(url)
-                extension = '.mp4'
-            elif bool(regex.search(r'gfycat\.com\/\w+', url)):
-                url = gfycat_url(url)
-                extension = '.mp4'
-            elif bool(regex.search(r'imgur', url)):
+            if bool(regex.search(r'imgur', url)):
                 reg = regex.search(r'(imgur.com\/)(\w+\/)?(\w+)(\.\w+)?(.*)?$', url)
                 domain, album, id, extension, bs = reg.groups()
                 logging.info('Imgur ID: ' + id)
-
                 if album:
-                    imgur_album(id)
+                    imgur_album(title, id)
                 else:
-                    imgur_image(id=id)
-                continue
-            elif text:
-                extension = '.txt'
-            name = title + extension
-            status = download_file(name, url, text)
-            logging.info(status)
+                    imgur_image(title, id=id)
+            else:
+                if submission.is_reddit_media_domain and submission.is_video:
+                    url = submission.media['reddit_video']['fallback_url']
+                    if submission.media['reddit_video']['is_gif']:
+                        extension = '.mp4'
+                    else:
+                        url_audio = regex.sub(r'\/[^\/]+$',r'/audio', url)
+                        status = download_video(title, url, url_audio)
+                        logging.info(status)
+                        continue
+                elif bool(regex.search(r'streamable\.com\/\w+', url)):
+                    url = streamable_url(url)
+                    extension = '.mp4'
+                elif bool(regex.search(r'gfycat\.com\/\w+', url)):
+                    url = gfycat_url(url)
+                    extension = '.mp4'
+                elif text:
+                    extension = '.txt'
+                if extension:
+                    name = title + extension
+                    status = download_file(name, url, text=text)
+                    logging.info(status)
 
-            url = 'https://www.reddit.com' + submission.permalink
-            text = '[InternetShortcut]\nURL=%s' % url
-            status = download_file(title_url, url, text)
-            logging.debug(status)
-
-            if get_size():
-                automation = False
-                raise Exception(f'\n{"*"*20}\n{storage} gigabytes reached.\n{"*"*20}\n')
+                url = 'https://www.reddit.com' + submission.permalink
+                text = '[InternetShortcut]\nURL=%s' % url
+                status = download_file(title_url, url, text=text)
+                logging.info(status)
+            gigs_percent = get_size(storage)
+            i += 1
+            if gigs_percent >= 100:
+                sys.stdout.write(f"\r100%")
+                print(f'\n{storage} gigabytes reached\n{i} posts downloaded')
+                return True
+            else:
+                post_percent = round(i * 100 / posts)
+                percent = max(post_percent, gigs_percent)
         except Exception as e:
-            print(f'Error: {e}')
+            logging.info(f'Error: {e}')
 
-def download_file(name, url, text):
+    sys.stdout.write(f"\r100%")
+    print(f'{i} post(s) downloaded')
+    return
+
+def download_file(name, url, text=None):
     try:
         if text:
             saveFile = open(name, 'w')
--- reddit_scrape.py
+++ reddit_scrape.py
@@ -1,79 +1,108 @@
-#! python3
+#!python3
 # Mehmet Hatip API Test
+try:
+    import os, shutil, logging, sys, download, configparser
+except e as Exception:
+    print(f'Error: {e}')
+    sys.exit()
 
-import os, shutil, logging, sys, download
+logging.basicConfig(
+filename='log.txt',
+filemode='w',
+level=logging.DEBUG,
+format='%(message)s')
 
-testing = True
+filename = os.path.abspath(
+os.path.join(os.path.dirname(__file__), 'settings.ini')
+)
+config = configparser.ConfigParser()
+config.read(filename)
+logging.info(f'Filename: \'{filename}\'')
 
-def get_size(start_path=os.getcwd()):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            # skip if it is symbolic link
-            if not os.path.islink(fp):
-                try:
-                    total_size += os.path.getsize(fp)
-                except:
-                    None
-    exceeded = bool(total_size >= storage * 10**9)
-    if exceeded:
-        print(msg_exit_format.format(f'Reached {storage} gigabyte(s)'))
-    return exceeded
 
-def settings(section, posts, storage):
-    choices = ['hot', 'top', 'new']
-    msg = f'Enter {", ".join(choices[:-1])}, or {choices[-1]} (Curr={section}): '
+DEFAULT = config['default']
+SUB_NAME = DEFAULT.get('sub_name', 'pics')
+SECTION = DEFAULT.get('section', 'hot')
+POSTS = DEFAULT.getint('posts', 10)
+STORAGE = DEFAULT.getfloat('storage', '.1')
+
+
+def settings(sub_name, section, posts, storage):
+
+    logging.info('\nStarted settings')
+    choices = ['hot', 'top', 'new', 'cont']
+    msg = [
+    f'Enter default subreddit (Curr={sub_name})',
+    f'Enter {", ".join(choices[:-1])}, or {choices[-1]} (Curr={section})',
+    f'Enter number of posts (Curr={posts})',
+    f'Enter maximum gigabyte capacity (Curr={storage})'
+    ]
+    logging.info(f'msg: "{msg}"')
+    max_width = len(max(msg, key=len))
+    temp = '{:<%s} : ' % str(max_width)
+
+    logging.info(f'Template: "{temp}"')
+    for i in range(len(msg)):
+        msg[i] = temp.format(msg[i])
+        logging.info(f'Message {i+1}: {msg[i]}')
+    sub_name = input(msg[0]) or sub_name
     while True:
-        section = input(msg).lower() or section
+        section = input(msg[1]).lower() or section
         if section in choices:
             break
-        print('Error: Input is not in options')
+        logging.info('Error: Input is not in options')
 
-    msg = f'Enter number of posts (Curr={posts}): '
     while True:
         try:
-            posts = int(input(msg) or posts)
+            posts = int(input(msg[2]) or posts)
             if posts < 1:
                 raise Exception
             break
         except:
             print('Error: Enter integer larger than 0')
 
-    msg = f'Enter maxmimum gigabyte capacity (Curr={storage}): '
     while True:
         try:
-            storage = float(input(msg) or storage)
+            storage = float(input(msg[3]) or storage)
             if storage <= 0:
                 raise Exception
             break
         except:
             print('Error: Enter float number larger than 0')
-    return section, posts, storage
+
+    global DEFAULT, SUB_NAME, SECTION, POSTS, STORAGE
+    DEFAULT['sub_name'], SUB_NAME = str(sub_name), sub_name
+    DEFAULT['section'], SECTION = str(section), section
+    DEFAULT['posts'], POSTS = str(posts), posts
+    DEFAULT['storage'], STORAGE = str(storage), storage
+    with open(filename, 'w') as fin:
+        config.write(fin)
+    logging.info('Exit settings\n')
 
 def prompt(automate):
-    msg = ("Enter name of subreddits, separate with space\n\t"
-    + "r for random\n\t"
+    msg = ("Enter name of subreddits, separate with commas\n\t"
+    + "r or press Enter for random\n\t"
     + "rr for random automation\n\t"
     + "del to delete subreddit folder\n\t"
     + "s for settings\n\t"
     + "e to exit\n")
 
     if not automate:
-        while True:
-            if len(sys.argv) <= 1:
-                sys.argv = sys.argv + input(msg).split()
-            try:
-                inp = sys.argv.pop(1)
-                break
-            except:
-                prompt=''
+        if len(sys.argv) <= 1:
+            sys.argv = sys.argv + input(msg).split(',')
+        try:
+            inp = sys.argv.pop(1)
+            if not inp:
+                inp = 'r'
+        except:
+            inp = SUB_NAME
     else:
         inp = 'r'
-    return inp
+    return inp.lower().strip()
 
-def delete_subreddit():
-    del_sub = input('Enter subreddit to be deleted: ').lower().strip()
+def delete_directory(del_sub = None):
+    if not del_sub:
+        del_sub = input('Enter subreddit to be deleted: ').lower().strip()
     try:
         shutil.rmtree(del_sub)
     except:
@@ -84,33 +113,47 @@ def delete_subreddit():
     else:
         return f'{del_sub} successfully deleted'
 
-def main():
-    download.log_start()
+def test():
+    delete_directory(del_sub=SUB_NAME)
+    os.chdir(data_file_name)
+    download.download_subreddit(SUB_NAME, SECTION, POSTS, STORAGE)
+    os.chdir('..')
+    os.startfile('.')
 
+def main():
+    if False:
+        test()
+        return
     data_file_name = 'Reddit scrape'
-    section, posts, storage = settings('top', 10, .1) if not testing else 'top', 10, .1
+    status = delete_directory(data_file_name)
+    logging.info(status)
     download.make_dir(data_file_name)
-    logging.debug('Start of while loop')
+    logging.info('Start of while loop')
     automate = False
     while True:
-        inp = prompt(automate)
-        logging.debug(f'Input: {inp}')
+        try:
+            inp = prompt(automate)
+            logging.debug(f'Input: {inp}')
 
-        if inp not in ['rr','del','s','e']:
-            download.download_subreddit(sub_name=inp, section=section, posts=posts)
-            while not os.path.basename(os.getcwd()) == data_file_name:
-                os.chdir("..")
-            print('Done\n')
-        elif inp == 'rr':
-            automate = True
-        elif inp == 'del':
-            status = delete_subreddit()
-            print(status)
-        elif inp == 's':
-            settings()
-        elif inp == 'e':
-            print('Exiting')
-            break
+            if inp not in ['rr','del','s','e']:
+                if download.download_subreddit(inp, SECTION, POSTS, STORAGE):
+                    automate = False
+                while not os.path.basename(os.getcwd()) == data_file_name:
+                    os.chdir("..")
+            elif inp == 'rr':
+                automate = True
+            elif inp == 'del':
+                status = delete_directory()
+                print(status)
+            elif inp == 's':
+                settings(SUB_NAME, SECTION, POSTS, STORAGE)
+            elif inp == 'e':
+                print('Exiting')
+                logging.info('**********\nEND HERE\n**********')
+                #os.startfile('.')
+                break
+        except e as Exception:
+            print(f'Error: {e}')
 
 if __name__ == '__main__':
     main()
